37cdfe285e6943e206b2eb6e71a32e64b224c007
Graham Cockell <graham.cockell@outlook.com>
2020-12-05 17:13:28 +0000

    Initial project files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OATControlWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ASCOM.DriverAccess.Telescope _telescope;
+        private OpenAstroTracker _oat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, RoutedEventArgs e)
+        {
+            var chooser = new ASCOM.Utilities.Chooser();
+            chooser.DeviceType = "Telescope";
+            String progID = chooser.Choose();
+            lblTelescope.Content = progID;
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            _oat = new OpenAstroTracker(txtComPort.Text);
+        }
+
+        private void txtSend_Click(object sender, RoutedEventArgs e)
+        {
+            String command = txtCommand.Text;
+            tbLog.Text += command + "\n";
+            String response = _oat.SendCommand(txtCommand.Text);
+            tbLog.Text += response + "\n";
+        }
+    }
+}
---
+++ OpenAstroTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace OATControlWPF
+{
+    class OpenAstroTracker
+    {
+        SerialPort _serial;
+
+        public OpenAstroTracker(String comPort)
+        {
+            _serial = new SerialPort();
+            _serial.PortName = comPort;
+            _serial.BaudRate = 57600;
+            _serial.DataBits = 8;
+            _serial.StopBits = StopBits.One;
+            _serial.Parity = Parity.None;
+            _serial.Handshake = Handshake.None;
+            _serial.ReadTimeout = 500;
+            _serial.WriteTimeout = 500;
+            _serial.Open();
+        }
+
+        public String SendCommand(String command)
+        {
+            _serial.Write(":" + command + "#");
+            String response = "";
+            try
+            {
+                while (true)
+                {
+                    int c = _serial.ReadChar();
+                    if (c == '#')
+                        break;
+                    response += (Char)c;
+                }
+            }
+            catch (TimeoutException)
+            {
+                response += "<timeout>"; // Return what we have
+            }
+            return response;
+        }
+    }
+}
